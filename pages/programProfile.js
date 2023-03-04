83f515e84f627b0cb1669b8a170241ba7115cf39
Allan Okoth <allanokoth695@gmail.com>
2023-03-04 03:43:34 +0200

    Update Next Pages

--- pages/programProfile.js
+++ pages/programProfile.js
@@ -42,79 +42,81 @@ const ProgramProfile = () => {
     }
 
     return (
-        <section className="bg-gray-200">
-            <div className="px-4 py-8 mx-auto">
-                <div className="lg:flex lg:-mx-2">
+        <>
+            <section className="bg-gray-200">
+                <div className="px-4 py-8 mx-auto">
+                    <div className="lg:flex lg:-mx-2">
 
-                    <div className="space-y-3 lg:w-1/5 lg:px-2 lg:space-y-4">
+                        <div className="space-y-3 lg:w-1/5 lg:px-2 lg:space-y-4">
 
-                        <div className="bg-white border border-white rounded-2xl p-2">
+                            <div className="bg-white border border-white rounded-2xl p-2">
 
-                            <div className="relative">
-                                <img
-                                    src={image}
-                                    alt=" "
-                                    className="w-full aspect-square object-cover rounded-2xl"
-                                />
+                                <div className="relative">
+                                    <img
+                                        src={image}
+                                        alt=" "
+                                        className="w-full aspect-square object-cover rounded-2xl"
+                                    />
 
-                                <div className="flex-auto sm:ml-5 justify-evenly">
-                                    <div className="flex items-center justify-between sm:mt-2">
-                                        <div className="flex items-center">
-                                            <div className="flex flex-col pt-2">
-                                                <div className="flex-none text-lg text-gray-800 font-bold leading-none">
-                                                    {title}
+                                    <div className="flex-auto sm:ml-5 justify-evenly">
+                                        <div className="flex items-center justify-between sm:mt-2">
+                                            <div className="flex items-center">
+                                                <div className="flex flex-col pt-2">
+                                                    <div className="flex-none text-lg text-gray-800 font-bold leading-none">
+                                                        {title}
+                                                    </div>
+                                                    <div className="flex-auto text-gray-500 pt-2">
+                                                        <span className="mr-3">{category}</span>
+                                                        <span className="mr-3 border-r border-gray-200  max-h-0" />
+                                                        <span>{year}</span>
+                                                    </div>
+                                                    <p className="w-96 text-sm text-slate-400 pt-2">{summary}</p>
                                                 </div>
-                                                <div className="flex-auto text-gray-500 pt-2">
-                                                    <span className="mr-3">{category}</span>
-                                                    <span className="mr-3 border-r border-gray-200  max-h-0" />
-                                                    <span>{year}</span>
-                                                </div>
-                                                <p className="w-96 text-sm text-slate-400 pt-2">{summary}</p>
                                             </div>
+                                        </div>
+
+                                        <div className="flex pt-4  text-sm text-gray-500">
+                                            <button onClick={() => router.push(`/createCertificate?pid=${pid}&image=${image}&title=${title}&summary=${summary}&year=${year}&uid=${uid}&category=${category}`)}
+                                                className="flex-no-shrink text-sm font-semibold bg-indigo-600 hover:bg-indigo-500 px-5 py-2 shadow-sm hover:shadow-lg tracking-wider text-white rounded-full transition ease-in duration-300">
+                                                Upload Certificate
+                                            </button>
                                         </div>
                                     </div>
 
-                                    <div className="flex pt-4  text-sm text-gray-500">
-                                        <button onClick={() => router.push(`/createCertificate?pid=${pid}&image=${image}&title=${title}&summary=${summary}&year=${year}&uid=${uid}&category=${category}`)}
-                                            className="flex-no-shrink text-sm font-semibold bg-indigo-600 hover:bg-indigo-500 px-5 py-2 shadow-sm hover:shadow-lg tracking-wider text-white rounded-full transition ease-in duration-300">
-                                            Upload Certificate
-                                        </button>
-                                    </div>
                                 </div>
+
 
                             </div>
 
 
                         </div>
+                        <div className="mt-6 lg:mt-0 lg:px-2 lg:w-4/5 ">
 
+                            <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-3">
+                                {certificates.map((certificate, i) => (
+                                    <div key={i} className="cursor-pointer rounded-xl bg-white p-3 shadow-lg hover:shadow-xl">
+                                        <div className="relative flex items-end overflow-hidden rounded-xl">
+                                            <img src={certificate.image} className="w-full aspect-[1.29/1] object-cover" alt=" " />
+                                        </div>
 
-                    </div>
-                    <div className="mt-6 lg:mt-0 lg:px-2 lg:w-4/5 ">
+                                        <div className="mt-1 p-2">
+                                            <h2 className="text-slate-700">{certificate.name}</h2>
+                                            <p className="mt-1 text-sm text-slate-400">{certificate.summary}</p>
 
-                        <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-3">
-                            {certificates.map((certificate, i) => (
-                                <div key={i} className="cursor-pointer rounded-xl bg-white p-3 shadow-lg hover:shadow-xl">
-                                    <div className="relative flex items-end overflow-hidden rounded-xl">
-                                        <img src={certificate.image} className="w-full aspect-[1.29/1] object-cover" alt=" " />
-                                    </div>
-
-                                    <div className="mt-1 p-2">
-                                        <h2 className="text-slate-700">{certificate.name}</h2>
-                                        <p className="mt-1 text-sm text-slate-400">{certificate.summary}</p>
-
-                                        <div className="mt-1 flex items-end justify-between">
-                                            <p>
-                                                <span className="text-sm text-orange-300">{certificate.year}</span>
-                                            </p>
+                                            <div className="mt-1 flex items-end justify-between">
+                                                <p>
+                                                    <span className="text-sm text-orange-300">{certificate.year}</span>
+                                                </p>
+                                            </div>
                                         </div>
                                     </div>
-                                </div>
-                            ))}
+                                ))}
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        </section>
+            </section>
+        </>
     )
 };
 
--- pages/programs.js
+++ pages/programs.js
@@ -59,35 +59,37 @@ const programs = () => {
     //if (loadingState === 'loaded' && !programs.length) return (<h1 className="py-10 px-20 text-3xl"> No Programs have been published</h1>)
 
     return (
-        <div className="bg-gray-200">
-            <ProgramBanner />
-            <div className="max-w-2xl mx-auto py-16 px-4 sm:py-4 sm:px-6 lg:max-w-7xl lg:px-8">
-                <div className="mt-1 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+        <>
+            <div className="bg-gray-200">
+                <ProgramBanner />
+                <div className="max-w-2xl mx-auto py-16 px-4 sm:py-4 sm:px-6 lg:max-w-7xl lg:px-8">
+                    <div className="mt-1 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
 
-                    {programList.map((program, i) => (
-                        <div key={i} className="cursor-pointer rounded-xl bg-white p-3 shadow-lg hover:shadow-xl">
-                            <div className="relative flex items-end overflow-hidden rounded-xl">
-                                <img src={program?.image} className="aspect-square w-full object-cover" alt=" " />
-                            </div>
-
-                            <div className="mt-1 p-2">
-                                <h2 className="text-slate-700 line-clamp-1">{program?.title}</h2>
-                                <p className="mt-1 text-sm text-slate-400 line-clamp-2">{program?.summary}</p>
-
-                                <div className="mt-3 flex items-end justify-between">
-                                    <p>
-                                        <span className="text-sm text-orange-300">Year: {program?.year}</span>
-                                    </p>
+                        {programList.map((program, i) => (
+                            <div key={i} className="cursor-pointer rounded-xl bg-white p-3 shadow-lg hover:shadow-xl">
+                                <div className="relative flex items-end overflow-hidden rounded-xl">
+                                    <img src={program?.image} className="aspect-square w-full object-cover" alt=" " />
                                 </div>
-                                <button onClick={() =>  router.push(`/programProfile?pid=${program.pid}&image=${program.image}&title=${program.title}&summary=${program.summary}&year=${program.year}&uid=${program.address}&category=${program.category}`)}
-                                    className="mt-4 py-2 px-10 w-full text-white font-semibold border border-indigo-700 rounded-xl md:rounded-full focus:ring focus:ring-indigo-300 bg-indigo-600 hover:bg-indigo-700 transition ease-in-out duration-200" type="button">Program Profile</button>
-                            </div>
-                        </div>
-                    ))}
 
+                                <div className="mt-1 p-2">
+                                    <h2 className="text-slate-700 line-clamp-1">{program?.title}</h2>
+                                    <p className="mt-1 text-sm text-slate-400 line-clamp-2">{program?.summary}</p>
+
+                                    <div className="mt-3 flex items-end justify-between">
+                                        <p>
+                                            <span className="text-sm text-orange-300">Year: {program?.year}</span>
+                                        </p>
+                                    </div>
+                                    <button onClick={() => router.push(`/programProfile?pid=${program.pid}&image=${program.image}&title=${program.title}&summary=${program.summary}&year=${program.year}&uid=${program.address}&category=${program.category}`)}
+                                        className="mt-4 py-2 px-10 w-full text-white font-semibold border border-indigo-700 rounded-xl md:rounded-full focus:ring focus:ring-indigo-300 bg-indigo-600 hover:bg-indigo-700 transition ease-in-out duration-200" type="button">Program Profile</button>
+                                </div>
+                            </div>
+                        ))}
+
+                    </div>
                 </div>
             </div>
-        </div>
+        </>
     )
 };
 
