6e5b7c3ec1e7e358299692d786a8db197906771e
Allan Okoth <allanokoth695@gmail.com>
2023-03-04 03:15:52 +0200

    Update Contact.js

--- pages/contact.js
+++ pages/contact.js
@@ -100,7 +100,7 @@ const contact = () => {
                                 Address
                             </h3>
                             <p className="font-medium max-w-xs mx-auto leading-relaxed">
-                                KK 242 Street, Kigali, Rwand
+                                KK 242 Street, Kigali, Rwanda
                             </p>
                         </div>
                     </div>
